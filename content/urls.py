f50f67a6375e76bdaf385afb6a2f10db941bd6e0
ThabangLigavha710 <thabangdanligavha@gmail.com>
2024-07-10 12:39:26 +0200

    latest update

--- content/urls.py
+++ content/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='vhashashe-home'),
     path('about', views.about, name='vhashashe-about'),
     path('services', views.services, name='vhashashe-services'),
-    path('about', views.contact, name='vhashashe-contact'),
+    path('contact', views.contact, name='vhashashe-contact'),
 ]
